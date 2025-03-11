2cf949e3e73899583d32b070fa09e5972977ebb7
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-10 23:56:37 -0400

    Migraciones

---
+++ Liamell_Cruz_P2_AP1/Migrations/20250311034836_Inicial.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Liamell_Cruz_P2_AP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ciudades",
+                columns: table => new
+                {
+                    CiudadId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Monto = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudades", x => x.CiudadId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Encuesta",
+                columns: table => new
+                {
+                    EncuestaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Asignatura = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Monto = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encuesta", x => x.EncuestaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EncuestaDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EncuestaId = table.Column<int>(type: "int", nullable: false),
+                    CiudadId = table.Column<int>(type: "int", nullable: false),
+                    Monto = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EncuestaDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_EncuestaDetalle_Ciudades_CiudadId",
+                        column: x => x.CiudadId,
+                        principalTable: "Ciudades",
+                        principalColumn: "CiudadId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EncuestaDetalle_Encuesta_EncuestaId",
+                        column: x => x.EncuestaId,
+                        principalTable: "Encuesta",
+                        principalColumn: "EncuestaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Ciudades",
+                columns: new[] { "CiudadId", "Monto", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, 0.0, "Tenares" },
+                    { 2, 0.0, "Salcedo" },
+                    { 3, 0.0, "San francisco" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EncuestaDetalle_CiudadId",
+                table: "EncuestaDetalle",
+                column: "CiudadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EncuestaDetalle_EncuestaId",
+                table: "EncuestaDetalle",
+                column: "EncuestaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EncuestaDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Ciudades");
+
+            migrationBuilder.DropTable(
+                name: "Encuesta");
+        }
+    }
+}
