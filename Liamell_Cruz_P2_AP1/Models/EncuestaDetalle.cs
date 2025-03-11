cc8c4294badba6f10d27e3946100909324ce8302
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-10 23:55:59 -0400

    Modelo detalle

---
+++ Liamell_Cruz_P2_AP1/Models/EncuestaDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Liamell_Cruz_P2_AP1.Models;
+
+public class EncuestaDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("Encuestas")]
+    public int EncuestaId { get; set; }
+
+    public Encuesta? Encuesta { get; set; }
+
+    [ForeignKey("Ciudades")]
+    public int CiudadId { get; set; }
+    public Ciudades? Ciudades { get; set; }
+
+    [Required(ErrorMessage = "Por favor ingrese el monto")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números enteros o decimales")]
+    public double Monto { get; set; }
+}
