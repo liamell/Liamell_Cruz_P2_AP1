eebc0e6d31838b1c9c1e041df4bc209e375d7b12
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-10 23:56:19 -0400

    Contexto

---
+++ Liamell_Cruz_P2_AP1/DAL/Contexto.cs
@@ -0,0 +1,33 @@
+﻿using Liamell_Cruz_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Liamell_Cruz_P2_AP1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+    public DbSet<Ciudades> Ciudades { get; set; }
+    public DbSet<Encuesta> Encuesta { get; set; }
+
+    public DbSet<EncuestaDetalle> EncuestaDetalle { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       
+        modelBuilder.Entity<Ciudades>().HasData(new List<Ciudades>()
+        {
+            new Ciudades() {CiudadId = 1, Nombre= "Tenares", Monto = 0},
+            new Ciudades() {CiudadId = 2, Nombre= "Salcedo", Monto = 0},
+            new Ciudades() {CiudadId = 3, Nombre= "San francisco", Monto = 0}
+
+        });
+    }
+
+
+
+}
+
+
