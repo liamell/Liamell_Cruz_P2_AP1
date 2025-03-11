3196e69ab83f4a42e060923201587eb1f2b43c13
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-10 23:59:28 -0400

    Inyecte el Service en el program

--- Liamell_Cruz_P2_AP1/Program.cs
+++ Liamell_Cruz_P2_AP1/Program.cs
@@ -1,10 +1,24 @@
 using Liamell_Cruz_P2_AP1.Components;
+using Liamell_Cruz_P2_AP1.DAL;
+using Liamell_Cruz_P2_AP1.Service;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddScoped<EncuestaService>();
+builder.Services.AddScoped<CiudadService>();
+builder.Services.AddScoped<DetalleService>();
+
+builder.Services.AddBlazorBootstrap();
+
+
 
 var app = builder.Build();
 
