24a1c4da68abdf242011686ddca425969bcf26b1
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-10 23:55:29 -0400

    Modelo Encuesta

---
+++ Liamell_Cruz_P2_AP1/Models/Encuesta.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Liamell_Cruz_P2_AP1.Models;
+
+public class Encuesta
+{
+    [Key]
+    public int EncuestaId { get; set; }
+
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+
+    [Required(ErrorMessage = "LA asignatura debe ser obligatoria")]
+    public string Asignatura { get; set; }
+
+    [Required(ErrorMessage = "Por favor ingrese el monto")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números")]
+    public double Monto { get; set; }
+
+
+    [ForeignKey("EncuestaId")]
+    public ICollection<EncuestaDetalle> encuestaDetalle { get; set; } = new List<EncuestaDetalle>();
+}
