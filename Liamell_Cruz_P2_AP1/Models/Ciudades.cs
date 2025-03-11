07f0f3841fac9d6c7dfbdd6b0b35118047544b2d
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-10 23:55:45 -0400

    Modelo Ciudades

---
+++ Liamell_Cruz_P2_AP1/Models/Ciudades.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Liamell_Cruz_P2_AP1.Models;
+
+public class Ciudades
+{
+    [Key]
+    
+        public int CiudadId { get; set; }
+
+        public string Nombre { get; set; }
+        public double Monto { get; set; }
+
+    
+
+
+
+}
+
+
